db7c4650d14fd20bd08824a2b8d5d1ac5893428f
Danail <danail.fx@gmail.com>
2026-02-13 02:33:44 +0200

    Add project files.

---
+++ CarHub/Data/CarHubDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarHub.Data
+{
+    public class CarHubDbContext : IdentityDbContext
+    {
+        public CarHubDbContext(DbContextOptions<CarHubDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ CarHub/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarHub.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
