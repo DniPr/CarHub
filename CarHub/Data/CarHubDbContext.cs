edfb7a7e32a7f55571a42d8b813c2cb0ca147bf1
Danail <danail.fx@gmail.com>
2026-02-13 03:05:18 +0200

    DbContext Configuration

--- CarHub/Data/CarHubDbContext.cs
+++ CarHub/Data/CarHubDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CarHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarHub.Data
@@ -8,6 +9,17 @@ namespace CarHub.Data
         public CarHubDbContext(DbContextOptions<CarHubDbContext> options)
             : base(options)
         {
+        }
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<CarAd> CarAds { get; set; } = null!;
+        public virtual DbSet<Favorite> FavoriteCarAds { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Favorite>()
+                .HasKey(f => new { f.UserId, f.CarAdId });
         }
     }
 }
